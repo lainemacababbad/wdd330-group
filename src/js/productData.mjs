449807d9fb5307dfaf8506d73e68bd4a02839c79
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2025-02-04 14:21:04 -0700

    Total in cart working now except for some items

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -16,3 +16,26 @@ export async function findProductById(id, category = "tents") { // this is just 
   const products = await getData(category);
   return products.find((item) => item.Id === id);
 }
+
+
+
+// previous code
+// function convertToJson(res) {
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Bad Response");
+//   }
+// }
+
+// export function getData(category = "tents") {
+//   return fetch(`../json/${category}.json`)
+//     .then(convertToJson)
+//     .then((data) => data);
+// }
+
+// export async function findProductById(id) {
+//   const products = await getData();
+//   return products.find((item) => item.Id === id);
+// }
+
