4d1c16a18d95871eeddeeccff4b17627a33c3874
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2025-02-25 18:05:33 -0700

    Accidentally deleted product-list, added it again

---
+++ src/js/product-list.js
@@ -0,0 +1,9 @@
+import ProductList from "./components/ProductList.svelte";
+import getParam from "./utils.mjs";
+
+const category = getParam("category");
+
+new ProductList({
+    target: document.querySelector(".products"),
+    props: { category: category },
+  });
