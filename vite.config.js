a88bfedd411cce3d927ff7385da499c7ec5cf99c
Jeremiah Franco <jeremiahfranco3@gmail.com>
2025-03-08 13:49:55 -0700

    Attempt at team 8

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resourceLimits } from "worker_threads";
 
 
 
@@ -14,6 +15,8 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+        order: resolve(__dirname, "src/orders/index.html"),
+        login: resolve(__dirname, "src/login/index.html"),
         product1: resolve(
           __dirname,
           "src/product_pages/cedar-ridge-rimrock-2.html"
